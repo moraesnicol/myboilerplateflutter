f10d831001036c3b2a7902fdfb22122cd0ae3a92
Gabriel Nicol <moraesnicol@gmail.com>
2020-12-06 03:27:10 -0300

    add columns

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -10,21 +10,47 @@ class HomePage extends StatefulWidget {
 
 class HomePageState extends State<HomePage> {
   int count = 0;
-  
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(
             title: Center(child: const Text('Que App Fdp')),
-            backgroundColor: Colors.amber),
-        body: Center(
-            child: Switch(
-          value: AppController.instance.isDartTheme,
-          onChanged: (value) {
-            AppController.instance.changeTheme();
-            }),
+            backgroundColor: Colors.amber,
+            actions: [
+              CustomSwitcher(),
+            ],
+        ),
+        body: Container(
+          width: double.infinity,
+          height: double.infinity,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,   //vertical
+            crossAxisAlignment: CrossAxisAlignment.stretch, //horizontal
+           
+           
+            children: [
+              
+              Text('Dark Mode'),
+              CustomSwitcher(),
+            
+
+            ],
+        ),
       )
     );
+  }
+}
+
+
+
+class CustomSwitcher extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Switch(
+        value: AppController.instance.isDartTheme,
+        onChanged: (value) {
+          AppController.instance.changeTheme();
+    });
   }
 }
