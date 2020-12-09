c72f370830abc172c866442825d401906712f405
Gabriel Nicol <moraesnicol@gmail.com>
2020-12-09 07:09:25 -0300

    .

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,50 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'app_widgets.dart';
+import 'package:helloworld/pages/viroses/viroses.dart';
+import 'package:provider/provider.dart';
 
-void main() {
+import 'controllers/authentications.dart';
+import 'pages/login/loginScreen.dart';
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(MyApp());
 }
 
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MultiProvider(
+      providers: [
+        Provider<AuthenticationService>(
+          create: (_) => AuthenticationService(FirebaseAuth.instance),
+        ),
+        StreamProvider(
+          create: (context) => context.read<AuthenticationService>().authStateChanges,
+        )
+      ],
+      child: MaterialApp(
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+          visualDensity: VisualDensity.adaptivePlatformDensity,
+        ),
+        home: AuthenticationWrapper(),
+      ),
+    );
+  }
+}
 
+class AuthenticationWrapper extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final firebaseUser = context.watch<User>();
 
+    if (firebaseUser != null) {
+      return Viroses();
+    }
+    return SignInPage();
+  }
+}
